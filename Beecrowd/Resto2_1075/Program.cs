6b3263fa0533283a2dd6dcdd365e66653249f2f6
Amanda Santos <amanda280721@gmail.com>
2024-01-23 20:01:32 -0400

    primeira solucao Resto2

---
+++ Beecrowd/Resto2_1075/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int pos = 1; pos <= 10000; pos++)
+        {
+            if (pos % n == 2)
+            {
+                Console.WriteLine(pos);
+            }
+        }
+    }
+}
