ddb8fb687ad11a5e84e5c39482e79b3a591cb501
Amanda Santos <amanda280721@gmail.com>
2024-01-24 11:44:43 -0400

    solucao Multiplos de 13

---
+++ Beecrowd/MultiplosDe13_1132/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MultiplosDe13_1132;
+
+public class Program
+{
+    static bool IsMultiplo13(int number)
+    {
+        return number % 13 == 0;
+    }
+
+    static void Main(string[] args)
+    {
+        int inicio = int.Parse(Console.ReadLine());
+        int fim = int.Parse(Console.ReadLine());
+
+        if (inicio > fim)
+        {
+            int aux = inicio;
+            inicio = fim;
+            fim = aux;
+        }
+
+        int soma = 0;
+
+        for (int i = inicio; i <= fim; i++)
+        {
+            if (!IsMultiplo13(i))
+            {
+                soma += i;
+            }
+        }
+
+        Console.WriteLine(soma);
+    }
+}
