7d4473508a5a095c6c9217e8d696f8baedae5d05
Amanda Santos <amanda280721@gmail.com>
2024-01-25 10:37:36 -0400

    Solucao DistanciaEntreDoisPontos

--- Beecrowd/DistanciaEntreDoisPontos_1015/obj/Debug/net6.0/DistanciaEntreDoisPontos_1015.AssemblyInfo.cs
+++ Beecrowd/DistanciaEntreDoisPontos_1015/obj/Debug/net6.0/DistanciaEntreDoisPontos_1015.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DistanciaEntreDoisPontos_1015")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01b9a765cd1c784732ccdd14adca778ca23a3f89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49df8784b86f840f8ddde7a771ad5c62a97dc29")]
 [assembly: System.Reflection.AssemblyProductAttribute("DistanciaEntreDoisPontos_1015")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DistanciaEntreDoisPontos_1015")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
